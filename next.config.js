b09da0cf204f3f3a40459b60fe62f13cb6d541c6
Caleb Luster <cwl117@outlook.com>
2022-08-27 12:12:30 -0500

    did alot if things some errors but we are working them out

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: [
+      'www.yamanashi-kankou.jp',
+      'lh3.googleusercontent.com'
+    
+    ],
+  }
 }
 
 module.exports = nextConfig
